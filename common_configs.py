843cb04529c9ca44bf12d7210939020302dd87a0
Jeff <jeffiscow2@gmail.com>
2023-11-17 16:41:55 +0000

    added 2 setting

---
+++ common_configs.py
@@ -0,0 +1,31 @@
+# varibals for commen configs
+
+sneaky_mode = input("Turn on sneaky mode? Y/N ")
+Mute_all = input("Bind key to " "  mute all ")
+# Drop_bomb = input("Bind key to " "  drop bomb ")
+# Jumpthrow_bind = input("Bind " " key to jumpthrow ")
+# Utility_binds = input("Bind flash to "" smoke "" HE to "" molly to "" ")
+# Mouse_wheel_jump = input("Turn on mouse wheel jump? Y/N ")
+# Show_Player_ID = input("Turn on show player ID? Y/N ")
+# Set_fps_cap = input("Set fps_max to "" ")
+# Disable_autohelp = input("Disable auto help? Y/N")
+# Set_max_allowable_ping = input("Max allowable ping " " ")
+
+
+
+if sneaky_mode == "Y":
+    print("ON")
+else:
+    print("OFF")
+
+
+if Mute_all == str:
+    print(Mute_all)
+else:
+    print(Mute_all)
+
+
+
+
+
+
