dd8adc6a841eb94dda00d7b92fa253d811c5e3dd
Jeff <jeffiscow2@gmail.com>
2023-11-20 15:20:18 +0000

    Added utli

--- common_configs.py
+++ common_configs.py
@@ -4,12 +4,18 @@ sneaky_mode = input("Turn on sneaky mode? Y/N ")
 Mute_all = input("Bind key to "   "  mute all ")
 Drop_bomb = input("Bind key to " "  drop bomb ")
 Jumpthrow_bind = input("Bind " " key to jumpthrow ")
-# Utility_binds = input("Bind flash to "" smoke "" HE to "" molly to "" ")
+
 Mouse_wheel_jump = input("Turn on mouse wheel jump? Y/N ")
 Show_Player_ID = input("Turn on show player ID? Y/N ")
 Set_fps_cap = input("Set fps_max to "" ")
 Disable_autohelp = input("Disable auto help? Y/N ")
 Set_max_allowable_ping = input("Max allowable ping " " ")
+
+# Ask the user to enter the keys for flash, smoke, HE, and Molly
+flash_key = input("Bind flash to: ")
+smoke_key = input("Bind smoke to: ")
+HE_key = input("Bind HE to: ")
+Molly_key = input("Bind Molly to: ")
 
 #need to fix so the print after each one is entered
 
@@ -47,6 +53,10 @@ if Disable_autohelp == "Y":
     print("disable autohelp  ON ")
 else:
     print("disable autohelp OFF ")
+
+# Print the user's choices
+print(f"You have bound flash to {flash_key}, smoke to {smoke_key}, HE to {HE_key}, and Molly to {Molly_key}.")
+
 
 
 
