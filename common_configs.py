159e54b2b4decacf85662c719c4fff13c3b3a157
Jeff <jeffiscow2@gmail.com>
2023-11-20 15:22:36 +0000

    black formatter added

--- common_configs.py
+++ common_configs.py
@@ -1,13 +1,13 @@
 # varibals for commen configs
 
 sneaky_mode = input("Turn on sneaky mode? Y/N ")
-Mute_all = input("Bind key to "   "  mute all ")
+Mute_all = input("Bind key to " "  mute all ")
 Drop_bomb = input("Bind key to " "  drop bomb ")
 Jumpthrow_bind = input("Bind " " key to jumpthrow ")
 
 Mouse_wheel_jump = input("Turn on mouse wheel jump? Y/N ")
 Show_Player_ID = input("Turn on show player ID? Y/N ")
-Set_fps_cap = input("Set fps_max to "" ")
+Set_fps_cap = input("Set fps_max to " " ")
 Disable_autohelp = input("Disable auto help? Y/N ")
 Set_max_allowable_ping = input("Max allowable ping " " ")
 
@@ -17,13 +17,12 @@ smoke_key = input("Bind smoke to: ")
 HE_key = input("Bind HE to: ")
 Molly_key = input("Bind Molly to: ")
 
-#need to fix so the print after each one is entered
+# need to fix so the print after each one is entered
 
 if sneaky_mode == "Y":
     print("sneaky mode ON ")
 else:
     print("sneaky mode OFF ")
-
 
 
 if Mute_all == Mute_all:
@@ -34,7 +33,6 @@ if Drop_bomb == Drop_bomb:
 
 if Jumpthrow_bind == Jumpthrow_bind:
     print("Mute all bound to " + Jumpthrow_bind)
-
 
 
 if Mouse_wheel_jump == "Y":
@@ -55,13 +53,6 @@ else:
     print("disable autohelp OFF ")
 
 # Print the user's choices
-print(f"You have bound flash to {flash_key}, smoke to {smoke_key}, HE to {HE_key}, and Molly to {Molly_key}.")
-
-
-
-
-
-
-
-
-
+print(
+    f"You have bound flash to {flash_key}, smoke to {smoke_key}, HE to {HE_key}, and Molly to {Molly_key}."
+)
