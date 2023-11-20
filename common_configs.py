d8d136b16f603fe6de9717f84fad41fa067de05e
Jeff <jeffiscow2@gmail.com>
2023-11-20 15:10:05 +0000

    added all the basic inputs

--- common_configs.py
+++ common_configs.py
@@ -1,28 +1,54 @@
 # varibals for commen configs
 
 sneaky_mode = input("Turn on sneaky mode? Y/N ")
-Mute_all = input("Bind key to " "  mute all ")
-# Drop_bomb = input("Bind key to " "  drop bomb ")
-# Jumpthrow_bind = input("Bind " " key to jumpthrow ")
+Mute_all = input("Bind key to "   "  mute all ")
+Drop_bomb = input("Bind key to " "  drop bomb ")
+Jumpthrow_bind = input("Bind " " key to jumpthrow ")
 # Utility_binds = input("Bind flash to "" smoke "" HE to "" molly to "" ")
-# Mouse_wheel_jump = input("Turn on mouse wheel jump? Y/N ")
-# Show_Player_ID = input("Turn on show player ID? Y/N ")
-# Set_fps_cap = input("Set fps_max to "" ")
-# Disable_autohelp = input("Disable auto help? Y/N")
-# Set_max_allowable_ping = input("Max allowable ping " " ")
+Mouse_wheel_jump = input("Turn on mouse wheel jump? Y/N ")
+Show_Player_ID = input("Turn on show player ID? Y/N ")
+Set_fps_cap = input("Set fps_max to "" ")
+Disable_autohelp = input("Disable auto help? Y/N ")
+Set_max_allowable_ping = input("Max allowable ping " " ")
 
 #need to fix so the print after each one is entered
 
 if sneaky_mode == "Y":
-    print("ON")
+    print("sneaky mode ON ")
 else:
-    print("OFF")
+    print("sneaky mode OFF ")
 
 
-if Mute_all == str:
-    print(Mute_all)
+
+if Mute_all == Mute_all:
+    print("Mute all bound to " + Mute_all)
+
+if Drop_bomb == Drop_bomb:
+    print("Mute all bound to " + Drop_bomb)
+
+if Jumpthrow_bind == Jumpthrow_bind:
+    print("Mute all bound to " + Jumpthrow_bind)
+
+
+
+if Mouse_wheel_jump == "Y":
+    print("Mousewheel bind ON ")
 else:
-    print(Mute_all)
+    print("Mousewheel bind OFF ")
+
+if Show_Player_ID == "Y":
+    print("Show player ID  ON ")
+else:
+    print("Show player ID OFF ")
+
+print(Set_fps_cap)
+
+if Disable_autohelp == "Y":
+    print("disable autohelp  ON ")
+else:
+    print("disable autohelp OFF ")
+
+
 
 
 
